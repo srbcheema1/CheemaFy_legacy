8b2657367bb5a8b5404083c8fd2ffe27c578ce04
Sarbjit Singh <srbcheema1@gmail.com>
2017-09-19 03:25:19 +0530

    updated bash_aliases and also created cheemafy

---
+++ cheemafy.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+#this file must be in ~/programs/myfiles/
+
+#copy home_files to its position
+cp -r ~/programs/myfiles/home_files/. ~/ 
+
+#copy folder srbScripts to  ~/programs/srbScript
+cp -r ~/programs/myfiles/srbScripts ~/programs/
---
+++ update_mf.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+#this must be in ~/programs/myfiles
+
+#cp .vimrc .bash_aliases .bashrc
+cp ~/.bashrc       ~/programs/home_files/
+cp ~/.bash_aliases ~/programs/home_files/
+cp ~/.vimrc        ~/programs/home_files/
+
+#cp srbScripts 
+
