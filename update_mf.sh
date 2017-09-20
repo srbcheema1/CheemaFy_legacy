1ae0e23e06e856c5adb79b97762b6d4a805e6af6
srbcheema1 <srbcheema1@gmail.com>
2017-09-21 03:37:26 +0530

    extended flexibility to operate on other computer

--- update_mf.sh
+++ update_mf.sh
@@ -6,6 +6,7 @@
 cp ~/.bashrc       ~/programs/myfiles/home_files/
 cp ~/.bash_aliases ~/programs/myfiles/home_files/
 cp ~/.vimrc        ~/programs/myfiles/home_files/
+cp ~/.gitconfig    ~/programs/myfiles/home_files/
 
 #cp srbScripts 
 cp -r ~/programs/srbScripts/. ~/programs/myfiles/srbScripts/
