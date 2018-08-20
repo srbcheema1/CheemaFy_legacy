ba2ee247591ec3267e499fc07bda92d846fba859
Sarbjit Singh <srbcheema1@gmail.com>
2018-08-21 02:17:53 +0530

    more about cp

--- myPlugins/default_codes/default.cpp
+++ myPlugins/default_codes/default.cpp
@@ -1,13 +1,5 @@
 #include<bits/stdc++.h>
 
-#define F0(i,t) for(int i=0; i<t; i++)
-#define F1(i,t) for(int i=1; i<=t; i++)
-
-#define Si(x) scanf("%d",&x)
-#define Si2(x,y) scanf("%d %d",&x,&y)
-#define Sl(x) scanf("%lld",&x)
-#define Sl2(x,y) scanf("%lld %lld",&x,&y)
-
 #define lc(n) ((n)<<1)
 #define rc(n) ((n)<<1|1)
 
