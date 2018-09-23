d920bc36f112b5d6970ed3f77d11dc679a290f6e
Sarbjit Singh <srbcheema1@gmail.com>
2018-09-24 02:47:00 +0530

    updated default cpp code

--- myPlugins/default_codes/default.simple.cpp
+++ myPlugins/default_codes/default.simple.cpp
@@ -1,6 +1,7 @@
 #include<bits/stdc++.h>
 #define dout if(debug and (debug_test == 0 or _t == debug_test) ) cout<<" "
 #define endl "\n"
+#define int long long
 using namespace std;
 
 #ifdef ONLINE_JUDGE
@@ -10,28 +11,28 @@ using namespace std;
 #endif
 int debug = DEBUG;// it will take care of itself on online-judge
 int debug_test = 0; // test_case to be debugged ... 0 means all ... -1 means none
-int _t = 0;
+int _t = 0;// test_case counter
 
 #define umap unordered_map
 #define pqueue priority_queue
-typedef unsigned long long int ulli;
-typedef unsigned int ui;
+typedef unsigned long long ulli;// rarely will be used
 typedef pair<int,int> mp;
 typedef vector<vector<int> > matrix; // matrix matt(n,vector<int>(m,0));
 
-const int mod = 1e9+7;
-
+template<class T>void _p(T x){int i=0;for(auto a: x){dout<<a<<",";if(i++ > 20)break;}}
 template<class T>int sz(T &x){return (int)x.size();}
 template<class T>void show(T first){dout<<first<<"\n";}
 template<class T,class... Args>void show(T first,Args... args){dout<<first;show(args...);}
-template<class T,class U>ostream &operator<<(ostream &os,pair<T,U> &x){return os<<x.first<<","<<x.second;}
-template<class T>void print(T first,T last){for(auto it = first;it != last;it++)dout<<*it;dout<<endl;}
-template<class T>void print(T container){print(container.begin(),container.end());}; // print(vec);
+template<class T,class U>ostream &operator<<(ostream &o,pair<T,U> &x){return o<<"("<<x.first<<","<<x.second<<")";}
+template<class T>ostream &operator<<(ostream &os,vector<T> &x){dout<<"[";_p(x);dout<<"] ";return os;}
+template<class T>ostream &operator<<(ostream &os,set<T> &x){dout<<"{";_p(x);dout<< "} ";return os;}
 template<class T>bool sort_fun(T a,T b){return a>b;} // for descending sort // templated
+
+const int mod = 1e9+7, inf = LLONG_MAX;
+int temp = -1,last = -1,hell=-1;
 struct Desc{bool operator()(int a,int b){return a>b;}}desc; // for descending sort
 
-int temp = -1,last = -1,hell=-1;
-bool good=true,bad=false;
+// code begins here
 
 void solve(){
     //dout << "Case #" << _t << ": " << endl;
@@ -41,7 +42,7 @@ void solve(){
     for(int i=0;i<n;i++){
         cin>>vec[i];
     }
-
+    //show("vec:",vec,"done");
 }
 
 signed main(){
@@ -51,3 +52,4 @@ signed main(){
     //cin>>test;
     for(int t = 1; t <= test; t++)_t = t,solve();
 }
+// a code by srbcheema1
