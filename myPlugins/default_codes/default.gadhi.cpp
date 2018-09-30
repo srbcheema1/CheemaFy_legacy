494833f34e18ebf84b6c93ce54961ba0d0dd0476
Sarbjit Singh <srbcheema1@gmail.com>
2018-10-01 05:05:43 +0530

    some work over compi and default codes

--- myPlugins/default_codes/default.gadhi.cpp
+++ myPlugins/default_codes/default.gadhi.cpp
@@ -1,17 +1,17 @@
 #include<bits/stdc++.h>
+#define int long long
 using namespace std;
 
-typedef long long int ulli;
 typedef pair<int,int> mp;
 
 const int mod = 1e9+7;
 
 void solve(){
-    
+
 }
 
 signed main(){
-    ios_base::sync_with_stdio(0);cin.tie(0);
+    //ios_base::sync_with_stdio(0);cin.tie(0);
     int t=1;
     // freopen("input","r",stdin);
     // cin>>t;
