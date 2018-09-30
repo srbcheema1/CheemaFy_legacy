546da63c107bf54fb29cfa9316b1fd6e7f07985b
Sarbjit Singh <srbcheema1@gmail.com>
2018-10-01 05:05:43 +0530

    some work over compi and default codes

--- myPlugins/default_codes/default.cpp
+++ myPlugins/default_codes/default.cpp
@@ -1,50 +1,60 @@
 #include<bits/stdc++.h>
-
-#define lc(n) ((n)<<1)
-#define rc(n) ((n)<<1|1)
-
-#define modu(a) (((a%mod)+mod)%mod)
-
-#define dout if(debugg) shift(),cout<<" "
-#define show(x) if(debugg) shift(),cout<<#x<<" := "<<x<<endl;
-
-   /* * * * * * * * * * * * * * * * * * * * * * * *
-    *                                             *
-    *            _/_/_/            _/             *
-    *         _/        _/  _/_/  _/_/_/          *
-    *          _/_/    _/_/      _/    _/         *
-    *             _/  _/        _/    _/          *
-    *      _/_/_/    _/        _/_/_/             *
-    *                                             *
-    * * * * * * * * * * * * * * * * * * * * * * * */
-
 using namespace std;
 
-typedef unsigned long long int ulli;
-typedef unsigned int ui;
+#define dout if(debug and (debug_test == 0 or _t == debug_test) ) cout<<" "
+#define endl "\n"
+#define int long long
+#define substring(i,j) substr(i,(j-i+1))
+
+#ifdef ONLINE_JUDGE
+#define DEBUG 0.0
+#else
+#define DEBUG 1
+#endif
+int debug = DEBUG;// it will take care of itself on online-judge
+int debug_test = 0; // test_case to be debugged ... 0 means all ... -1 means none
+int _t = 0;// test_case counter
+
+#define umap unordered_map
+#define pqueue priority_queue
+typedef unsigned long long ulli;// rarely will be used
 typedef pair<int,int> mp;
-typedef vector<vector<int> > matrix;
+typedef vector<vector<int> > matrix; // matrix matt(n,vector<int>(m,0));
 
-int dx[] = {0,0,1,-1};
-int dy[] = {1,-1,0,0};
-const int mod = 1e9+7;
+template<class T>void _p(T x){int i=0;for(auto a: x){dout<<a<<",";if(i++ > 20)break;}}
+template<class T>int sz(T &x){return (int)x.size();}
+template<class T>void show(T first){dout<<first<<"\n";}
+template<class T,class... Args>void show(T first,Args... args){dout<<first;show(args...);}
+template<class T,class U>ostream &operator<<(ostream &o,pair<T,U> &x){return o<<"("<<x.first<<","<<x.second<<")";}
+template<class T>ostream &operator<<(ostream &os,vector<T> &x){dout<<"[";_p(x);dout<<"] ";return os;}
+template<class T>ostream &operator<<(ostream &os,set<T> &x){dout<<"{";_p(x);dout<< "} ";return os;}
+template<class T>bool sort_fun(T a,T b){return a>b;} // for descending sort // templated
 
-int debugg = 0;
+const int mod = 1e9+7, inf = LLONG_MAX;
+int temp = -1,last = -1,hell=-1;
+struct Desc{bool operator()(int a,int b){return a>b;}}desc; // for descending sort
 
-int ss=0;
-void shift(){
-    if(debugg)
-        for(int i=0;i<ss;i++)
-            cout<<".";
-}
+// code begins here
 
 void solve(){
-
+    //dout << "Case #" << _t << ": " << endl;
+    int n;
+    cin>>n;
+    vector<int> vec(n);
+    for(int i=0;i<n;i++){
+        cin>>vec[i];
+    }
+    //show("vec:",vec,"done");
 }
+
 signed main(){
-    int t=1;
+    #ifdef ONLINE_JUDGE
     ios_base::sync_with_stdio(0);cin.tie(0);
-    cin>>t;
-    while(t--)solve();
+    #else
+    //freopen("input","r",stdin);
+    #endif
+    int test=1;
+    //cin>>test;
+    for(int t = 1; t <= test; t++)_t = t,solve();
 }
 // a code by srbcheema1
