60da0c467f94edd20361d80fe555c9b552972224
Sarbjit Singh <srbcheema1@gmail.com>
2018-10-07 22:58:48 +0530

    ignored coolkit files and updated default.cpp

--- myPlugins/default_codes/default.cpp
+++ myPlugins/default_codes/default.cpp
@@ -4,7 +4,7 @@ using namespace std;
 #define dout if(debug and (debug_test == 0 or _t == debug_test) ) cout<<" "
 #define endl "\n"
 #define int long long
-#define substring(i,j) substr(i,(j-i+1))
+#define substring(i,j) substr(i,(j-i+1)) // both i,j are included
 
 #ifdef ONLINE_JUDGE
 #define DEBUG 0.0
@@ -21,14 +21,17 @@ typedef unsigned long long ulli;// rarely will be used
 typedef pair<int,int> mp;
 typedef vector<vector<int> > matrix; // matrix matt(n,vector<int>(m,0));
 
-template<class T>void _p(T x){int i=0;for(auto a: x){dout<<a<<",";if(i++ > 20)break;}}
 template<class T>int sz(T &x){return (int)x.size();}
 template<class T>void show(T first){dout<<first<<"\n";}
+template<class T>bool sort_fun(T a,T b){return a>b;} // for descending sort // templated
+template<class T,class U>bool found(T t,U u){if(t.find(u)==t.end())return false;return true;}
+
+// helping templates ignore them
 template<class T,class... Args>void show(T first,Args... args){dout<<first;show(args...);}
+template<class T>void _p(T x){int i=0;for(auto a: x){dout<<a<<",";if(i++ > 20)break;}}
 template<class T,class U>ostream &operator<<(ostream &o,pair<T,U> &x){return o<<"("<<x.first<<","<<x.second<<")";}
 template<class T>ostream &operator<<(ostream &os,vector<T> &x){dout<<"[";_p(x);dout<<"] ";return os;}
 template<class T>ostream &operator<<(ostream &os,set<T> &x){dout<<"{";_p(x);dout<< "} ";return os;}
-template<class T>bool sort_fun(T a,T b){return a>b;} // for descending sort // templated
 
 const int mod = 1e9+7, inf = LLONG_MAX;
 int temp = -1,last = -1,hell=-1;
